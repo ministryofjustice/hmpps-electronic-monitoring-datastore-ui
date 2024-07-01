e36d09d2dc3a56034676cd4267b7a26d0e7be3e1
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-07-01 14:58:45 +0100

    Add tests for search page components.

--- integration_tests/e2e/search.cy.ts
+++ integration_tests/e2e/search.cy.ts
@@ -12,20 +12,109 @@ context('Search', () => {
     it('Subject ID field renders', () => {
       const searchPage = Page.verifyOnPage(SearchPage)
       searchPage.subjectIdField().should('be.visible')
-      // searchPage.headerUserName().should('contain.text', 'J. Smith')
     })
 
     it('Subject ID label renders', () => {
       const searchPage = Page.verifyOnPage(SearchPage)
       searchPage.subjectIdLabel().should('be.visible')
       searchPage.subjectIdLabel().should('be.visible')
-      // searchPage.headerUserName().should('contain.text', 'J. Smith')
     })
 
     it('Subject ID label text is correct', () => {
       const searchPage = Page.verifyOnPage(SearchPage)
       searchPage.subjectIdLabel().should('contain', 'Subject ID')
-      // searchPage.headerUserName().should('contain.text', 'J. Smith')
+    })
+  })
+
+  describe('First name field', () => {
+    it('First name field renders', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.firstNameField().should('be.visible')
+    })
+
+    it('First name label renders', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.firstNameLabel().should('be.visible')
+      searchPage.firstNameLabel().should('be.visible')
+    })
+
+    it('First name label text is correct', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.firstNameLabel().should('contain', 'First name')
+    })
+  })
+
+  describe('Last name field', () => {
+    it('Last name field renders', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.lastNameField().should('be.visible')
+    })
+
+    it('Last name label renders', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.lastNameLabel().should('be.visible')
+      searchPage.lastNameLabel().should('be.visible')
+    })
+
+    it('Last name label text is correct', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.lastNameLabel().should('contain', 'Last name')
+    })
+  })
+
+  describe('Date of birth fields', () => {
+    it('Date of birth fields render', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.dayOfBirthField().should('be.visible')
+      searchPage.yearOfBirthField().should('be.visible')
+      searchPage.monthOfBirthField().should('be.visible')
+    })
+
+    it('Date of birth label texts are correct', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.dayOfBirthLabel().should('contain', 'Day')
+      searchPage.monthOfBirthLabel().should('contain', 'Month')
+      searchPage.yearOfBirthLabel().should('contain', 'Year')
+    })
+
+    it('Date of birth legend renders', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.dateOfBirthLegend().should('be.visible')
+    })
+
+    it('Date of birth legend text is correct', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.dateOfBirthLegend().should('contain', 'Date of birth')
+    })
+  })
+
+  describe('Alias field', () => {
+    it('Alias field renders', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.aliasField().should('be.visible')
+    })
+
+    it('Alias label renders', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.aliasLabel().should('be.visible')
+      searchPage.aliasLabel().should('be.visible')
+    })
+
+    it('Alias label text is correct', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.aliasLabel().should('contain', 'Alias')
+    })
+  })
+
+  describe('Search button', () => {
+    it('Search button renders', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.searchButton().should('be.visible')
+    })
+
+    it('Search button text is correct', () => {
+      const searchPage = Page.verifyOnPage(SearchPage)
+      searchPage.searchButton().should('contain', 'Search')
     })
   })
 })
--- integration_tests/pages/search.ts
+++ integration_tests/pages/search.ts
@@ -8,4 +8,32 @@ export default class SearchPage extends Page {
   subjectIdField = (): PageElement => cy.get('#subject-id')
 
   subjectIdLabel = (): PageElement => cy.get('#subject-id').prev('label')
+
+  firstNameField = (): PageElement => cy.get('#first-name')
+
+  firstNameLabel = (): PageElement => cy.get('#first-name').prev('label')
+
+  lastNameField = (): PageElement => cy.get('#last-name')
+
+  lastNameLabel = (): PageElement => cy.get('#last-name').prev('label')
+
+  dateOfBirthLegend = (): PageElement => cy.get('#date-of-birth').prev('legend')
+
+  dayOfBirthField = (): PageElement => cy.get('#date-of-birth-day')
+
+  dayOfBirthLabel = (): PageElement => cy.get('#date-of-birth-day').prev('label')
+
+  monthOfBirthField = (): PageElement => cy.get('#date-of-birth-month')
+
+  monthOfBirthLabel = (): PageElement => cy.get('#date-of-birth-month').prev('label')
+
+  yearOfBirthField = (): PageElement => cy.get('#date-of-birth-year')
+
+  yearOfBirthLabel = (): PageElement => cy.get('#date-of-birth-year').prev('label')
+
+  aliasField = (): PageElement => cy.get('#alias')
+
+  aliasLabel = (): PageElement => cy.get('#alias').prev('label')
+
+  searchButton = (): PageElement => cy.get('#case-search-form').children('button')
 }
