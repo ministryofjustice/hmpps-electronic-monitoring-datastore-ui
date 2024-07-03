199a9a267407232f4a05ccbf2f43cee26babb025
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-07-03 17:11:19 +0100

    Configure jquery so that moj components function.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,10 @@
+import $ from 'jquery'
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
+
+// JQuery required for mojFrontend.
+// https://design-patterns.service.justice.gov.uk/get-started/setting-up-javascript/
+window.$ = $
 
 govukFrontend.initAll()
 mojFrontend.initAll()
