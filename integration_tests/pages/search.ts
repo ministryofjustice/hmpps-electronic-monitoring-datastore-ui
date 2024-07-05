2ad3b5f246c99f856f3f2c0fac80cb38ab3bf5b4
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-07-05 15:16:02 +0100

    Add validation styling to search page components, with mock data for dev testing.

--- integration_tests/pages/search.ts
+++ integration_tests/pages/search.ts
@@ -11,33 +11,33 @@ export default class SearchPage extends Page {
 
   subjectIdField = (): PageElement => cy.get('#subject-id')
 
-  subjectIdLabel = (): PageElement => cy.get('#subject-id').prev('label')
+  subjectIdLabel = (): PageElement => cy.get('#subject-id').siblings('label')
 
   firstNameField = (): PageElement => cy.get('#first-name')
 
-  firstNameLabel = (): PageElement => cy.get('#first-name').prev('label')
+  firstNameLabel = (): PageElement => cy.get('#first-name').siblings('label')
 
   lastNameField = (): PageElement => cy.get('#last-name')
 
-  lastNameLabel = (): PageElement => cy.get('#last-name').prev('label')
+  lastNameLabel = (): PageElement => cy.get('#last-name').siblings('label')
 
-  dateOfBirthLegend = (): PageElement => cy.get('#date-of-birth').prev('legend')
+  dateOfBirthLegend = (): PageElement => cy.get('#date-of-birth').siblings('legend')
 
   dayOfBirthField = (): PageElement => cy.get('#date-of-birth-day')
 
-  dayOfBirthLabel = (): PageElement => cy.get('#date-of-birth-day').prev('label')
+  dayOfBirthLabel = (): PageElement => cy.get('#date-of-birth-day').siblings('label')
 
   monthOfBirthField = (): PageElement => cy.get('#date-of-birth-month')
 
-  monthOfBirthLabel = (): PageElement => cy.get('#date-of-birth-month').prev('label')
+  monthOfBirthLabel = (): PageElement => cy.get('#date-of-birth-month').siblings('label')
 
   yearOfBirthField = (): PageElement => cy.get('#date-of-birth-year')
 
-  yearOfBirthLabel = (): PageElement => cy.get('#date-of-birth-year').prev('label')
+  yearOfBirthLabel = (): PageElement => cy.get('#date-of-birth-year').siblings('label')
 
   aliasField = (): PageElement => cy.get('#alias')
 
-  aliasLabel = (): PageElement => cy.get('#alias').prev('label')
+  aliasLabel = (): PageElement => cy.get('#alias').siblings('label')
 
   searchButton = (): PageElement => cy.get('#case-search-form').children('button')
 }
