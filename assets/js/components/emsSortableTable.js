fddc384239ffaa640707ec6fd5b518d256cf5250
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-07-16 15:52:27 +0100

    The sortable table's pagination menu buttons can be navigated, and are hidden, displayed & highlighted correctly. Pagination doesn't yet change the content shown in the table.

--- assets/js/components/emsSortableTable.js
+++ assets/js/components/emsSortableTable.js
@@ -2,27 +2,95 @@ function init() {
   const emsSortableTables = document.getElementsByClassName('ems-sortable-table')
 
   for (let table of emsSortableTables) {
-    const totalRecords = table.dataset.totalrecords
-    const pageSize = table.dataset.pagesize
-    const totalPages = table.dataset.totalpages
+    const totalRecords = parseInt(table.dataset.totalrecords)
+    const totalPages = parseInt(table.dataset.totalpages)
+    const pageSize = parseInt(table.dataset.pagesize)
+    let currentPage = parseInt(table.dataset.currentpage)
 
-    const rows = table.getElementsByClassName('govuk-table__row')
-    for (let row of rows) {
-      row.classList.remove('ems-ui-table--row__hidden')
-      row.classList.add('ems-ui-table--row')
+    const pagination = table.querySelector('.govuk-pagination')
+    pagination.classList.add('test-m')
+    const prevButton = pagination.querySelector('.govuk-pagination__prev')
+    const nextButton = pagination.querySelector('.govuk-pagination__next')
+    const pageButtons = pagination.getElementsByClassName('govuk-pagination__item--link')
+    const ellipses = pagination.getElementsByClassName('govuk-pagination__item--ellipses')
+
+    // Show the correct pagination buttons based on the current page. Used to update the pagination component when the page is changed.
+    const updatePagination = page => {
+      // Show or hide previous & next buttons
+      page == 1 ? prevButton.classList.add('hidden') : prevButton.classList.remove('hidden')
+
+      page == totalPages ? nextButton.classList.add('hidden') : nextButton.classList.remove('hidden')
+
+      // Show or hide ellipses
+      if (totalPages > 5) {
+        page < 4 ? ellipses[0].classList.add('hidden') : ellipses[0].classList.remove('hidden')
+
+        page > totalPages - 3 ? ellipses[1].classList.add('hidden') : ellipses[1].classList.remove('hidden')
+      }
+
+      // Show or hide page number buttons
+      for (let button of pageButtons) {
+        const buttonNumber = parseInt(button.dataset.buttonnumber)
+
+        // Highlight the active page number
+        buttonNumber == page
+          ? button.classList.add('govuk-pagination__item--current')
+          : button.classList.remove('govuk-pagination__item--current')
+
+        // Hide all page buttons except the first and last ones, which are always visible
+        if (buttonNumber != 1 && buttonNumber != totalPages) {
+          button.classList.add('hidden')
+        }
+
+        // Show the buttons for the current page ±1
+        if (buttonNumber == page || buttonNumber == page + 1 || buttonNumber == page - 1) {
+          button.classList.remove('hidden')
+        }
+
+        // If the first or last page is selected, show the first or last three page buttons.
+        if (page == 1 && buttonNumber == 3) {
+          button.classList.remove('hidden')
+        }
+        if (page == totalPages && buttonNumber == totalPages - 2) {
+          button.classList.remove('hidden')
+        }
+      }
     }
 
-    const paginationMenu = table.getElementsByClassName('govuk-pagination')[0]
-    const paginationLinks = paginationMenu.getElementsByClassName('govuk-pagination__link')
-
-    for (let link of paginationLinks) {
-      link.classList.add('ems-ui-table--row')
-      link.addEventListener('click', function (event) {
+    // Function to initialise the pagination buttons
+    const initialisePaginationButtons = () => {
+      prevButton.addEventListener('click', function (event) {
         event.preventDefault()
-        console.log('Pagination button registers click')
+        if (currentPage != 1) {
+          currentPage--
+          updatePagination(currentPage)
+        }
         return false
       })
+
+      nextButton.addEventListener('click', function (event) {
+        event.preventDefault()
+        if (currentPage != totalPages) {
+          currentPage++
+          updatePagination(currentPage)
+        }
+        return false
+      })
+
+      for (let button of pageButtons) {
+        button.addEventListener('click', function (event) {
+          event.preventDefault()
+          const newPage = parseInt(button.dataset.buttonnumber)
+          currentPage = newPage
+          updatePagination(newPage)
+          return false
+        })
+      }
     }
+
+    // Initialise pagination
+    updatePagination(currentPage)
+    initialisePaginationButtons()
   }
 }
 
