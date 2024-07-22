9eade1d170bd39d6b8c1613352417769d0fdfa26
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-07-22 16:52:02 +0100

    Continue work on date input vailidation (WIP).

--- assets/js/components/emsDateFilter.js
+++ assets/js/components/emsDateFilter.js
@@ -1,34 +1,58 @@
+import { validateDate } from '../utils/validateDate.js'
+
 function init() {
   const emsDateFilters = document.getElementsByClassName('ems-date-filter')
 
   for (let dateFilter of emsDateFilters) {
     const applyFilter = () => {
-      // Get the start and end dates
-      const getDateString = dateInputs => {
-        return `${dateInputs[2].value + dateInputs[1].value + dateInputs[0].value}`
-      }
+      //Validate dates
+      const startDateInputs = dateFilter.querySelector('#start-date').querySelectorAll('input')
+      const endDateInputs = dateFilter.querySelector('#end-date').querySelectorAll('input')
 
-      const startDate = getDateString(dateFilter.querySelector('#start-date').querySelectorAll('input'))
-      const endDate = getDateString(dateFilter.querySelector('#end-date').querySelectorAll('input'))
+      const validatedStartDate = validateDate(
+        startDateInputs[0].value,
+        startDateInputs[1].value,
+        startDateInputs[2].value,
+        'Start date',
+        true,
+      )
+
+      const validatedEndDate = validateDate(
+        endDateInputs[0].value,
+        endDateInputs[1].value,
+        endDateInputs[2].value,
+        'End date',
+        false,
+      )
 
       // Filter the records
       const filterableTables = document.querySelectorAll('.date-filterable')
 
-      for (let table of filterableTables) {
-        const rows = table.querySelectorAll('[data-filter-date]')
+      if (validatedStartDate.error || validatedEndDate.error) {
+        console.log('Date validation failed. Filter was not applied.')
+        console.log(
+          validatedStartDate.error ? 'Start date error: ' + validatedStartDate.error : 'No error in start date.',
+        )
+        console.log(validatedEndDate.error ? 'End date error: ' + validatedEndDate.error : 'No error in end date.')
+      } else {
+        for (let table of filterableTables) {
+          const rows = table.querySelectorAll('[data-filter-date]')
 
-        for (let row of rows) {
-          const filterDate = parseInt(row.dataset.filterDate)
+          for (let row of rows) {
+            const filterDate = parseInt(row.dataset.filterDate)
+            const startDate = validatedStartDate.dateString
+            const endDate = validatedEndDate.dateString
 
-          if (filterDate < startDate || filterDate >= endDate) {
-            row.classList.add('filter-active')
-          } else {
-            row.classList.remove('filter-active')
+            if (filterDate < startDate || filterDate > endDate) {
+              row.classList.add('filter-active')
+            } else {
+              row.classList.remove('filter-active')
+            }
           }
-        }
 
-        // Invoke the table's pagination, returning it to page 1 and refreshing the results table.
-        table.getElementsByClassName('moj-pagination__item--link')[0].click()
+          // Invoke the table's pagination, returning it to page 1 and refreshing the results table.
+          table.getElementsByClassName('moj-pagination__item--link')[0].click()
+        }
       }
     }
 
--- assets/js/utils/validateDate.js
+++ assets/js/utils/validateDate.js
@@ -1,33 +1,46 @@
 function validateDate(day, month, year, dateName, isPast) {
   const date = {
-    value: new Date(`${year + '/' + month + '/' + day}`),
+    dateStamp: new Date(`${year + '/' + month + '/' + day}`),
+    dateString: null,
     error: null,
   }
 
   const now = new Date().setHours(0, 0, 0, 0)
 
-  if ((day == null) & (month == null) & (year == null)) {
+  const isNumericalString = string => string.match(/^\d+$/)
+
+  if (!day & !month & !year) {
     date.error = 'Enter a date.'
     return date
   }
 
-  if (day == null) {
+  if (isNaN(date.dateStamp)) {
+    date.error = `${dateName} must be a real date.`
+    return date
+  }
+
+  if (!day) {
     date.error = 'Enter a day.'
     return date
   }
 
-  if (month == null) {
+  if (!month) {
     date.error = 'Enter a month.'
     return date
   }
 
-  if (year == null) {
+  if (!year) {
     date.error = 'Enter a year.'
     return date
   }
 
-  if (isNaN(date.value)) {
+  if (!isNumericalString(day) || !isNumericalString(month) || !isNumericalString(year)) {
     date.error = `${dateName} must be a real date.`
+    return date
+  }
+
+  if (year && year.length != 4) {
+    date.error = `Enter a full year (eg. 2001).`
     return date
   }
 
@@ -35,6 +48,12 @@ function validateDate(day, month, year, dateName, isPast) {
     date.error = `${dateName} must be in the past.`
     return date
   }
+
+  // If validation is successful,
+  // Convert the input date to a time string in format YYYYMMDD & return it
+  const offset = date.dateStamp.getTimezoneOffset()
+  const offsetDate = new Date(date.dateStamp.getTime() - offset * 60 * 1000)
+  date.dateString = offsetDate.toISOString().split('T')[0].replaceAll('-', '')
 
   return date
 }
