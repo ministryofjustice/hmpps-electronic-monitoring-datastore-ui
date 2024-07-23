e81f2b1b25121dec459780102ea7fdbc8857f858
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-07-23 12:13:12 +0100

    Date validation
    EMS date filter: If only a start date is entered, then the records after that date are shown, & vice versa if only an end date is entered.
    
    EMS date filter: Implement the clear filters button.
    
    EMS date filter: Correct a SASS error that prevented filtered rows from being hidden.
    
    Update valdateDate utility to return a string indicating which date field produced the error message.
    
    Refactor valdateDate utility.
    
    Create a date range validation utility.
    
    Add input validation error message & styling to ems date filter component.
    
    Refactor date input validation.

--- assets/js/components/emsDateFilter.js
+++ assets/js/components/emsDateFilter.js
@@ -1,53 +1,89 @@
-import { validateDate } from '../utils/validateDate.js'
+import { validateDateRange } from '../utils/validateDateRange.js'
 
 function init() {
   const emsDateFilters = document.getElementsByClassName('ems-date-filter')
 
   for (let dateFilter of emsDateFilters) {
+    // Get buttons
+    const filterButton = dateFilter.querySelector('.ems-date-filter__filter-button')
+    const clearFiltersButton = dateFilter.querySelector('.ems-date-filter__clear-filters-button')
+
+    // Get filter date elements
+    const startDateFields = dateFilter.querySelector('#start-date').querySelectorAll('input')
+    const endDateFields = dateFilter.querySelector('#end-date').querySelectorAll('input')
+    const errorMessage = dateFilter.querySelector('.govuk-error-message')
+
+    //Get all filterable tables within the page
+    const filterableTables = document.querySelectorAll('.date-filterable')
+
+    // Method for the filter button
     const applyFilter = () => {
       //Validate dates
-      const startDateInputs = dateFilter.querySelector('#start-date').querySelectorAll('input')
-      const endDateInputs = dateFilter.querySelector('#end-date').querySelectorAll('input')
+      const startDateInputs = {
+        day: startDateFields[0].value,
+        month: startDateFields[1].value,
+        year: startDateFields[2].value,
+        dateName: 'Start date',
+        isPast: true,
+        isMandatory: false,
+      }
 
-      const validatedStartDate = validateDate(
-        startDateInputs[0].value,
-        startDateInputs[1].value,
-        startDateInputs[2].value,
-        'Start date',
-        true,
-      )
+      const endDateInputs = {
+        day: endDateFields[0].value,
+        month: endDateFields[1].value,
+        year: endDateFields[2].value,
+        dateName: 'End date',
+        isPast: false,
+        isMandatory: false,
+      }
 
-      const validatedEndDate = validateDate(
-        endDateInputs[0].value,
-        endDateInputs[1].value,
-        endDateInputs[2].value,
-        'End date',
-        false,
-      )
+      const {
+        startDate: startDate,
+        endDate: endDate,
+        error: dateRangeError,
+        errorFields,
+      } = validateDateRange(startDateInputs, endDateInputs)
 
-      // Filter the records
-      const filterableTables = document.querySelectorAll('.date-filterable')
+      // If input validation fails, show error message & styling.
+      // Else, remove error messge & styling & filter the table.
+      if (dateRangeError) {
+        dateFilter.classList.add('ems-date-filter--error')
+        errorMessage.classList.remove('hidden')
+        errorMessage.textContent = dateRangeError
 
-      if (validatedStartDate.error || validatedEndDate.error) {
-        console.log('Date validation failed. Filter was not applied.')
-        console.log(
-          validatedStartDate.error ? 'Start date error: ' + validatedStartDate.error : 'No error in start date.',
-        )
-        console.log(validatedEndDate.error ? 'End date error: ' + validatedEndDate.error : 'No error in end date.')
+        for (let field of dateFilter.querySelectorAll('.govuk-input')) {
+          field.classList.remove('govuk-input--error')
+        }
+        errorFields.forEach(field => dateFilter.querySelector(`#${field}`).classList.add('govuk-input--error'))
       } else {
+        dateFilter.classList.remove('ems-date-filter--error')
+        errorMessage.classList.add('hidden')
+        errorMessage.textContent = ''
+        for (let field of dateFilter.querySelectorAll('.govuk-input')) {
+          field.classList.remove('govuk-input--error')
+        }
+
+        // Filter the records if there are no validation errors.
         for (let table of filterableTables) {
           const rows = table.querySelectorAll('[data-filter-date]')
 
           for (let row of rows) {
             const filterDate = parseInt(row.dataset.filterDate)
-            const startDate = validatedStartDate.dateString
-            const endDate = validatedEndDate.dateString
+            // const startDate = startDateString
+            // const endDate = endDateString
 
-            if (filterDate < startDate || filterDate > endDate) {
-              row.classList.add('filter-active')
-            } else {
+            if ((startDate == null || filterDate > startDate) && (endDate == null || filterDate < endDate)) {
               row.classList.remove('filter-active')
+            } else {
+              row.classList.add('filter-active')
             }
+          }
+
+          // If any records are filtered out, show the clear filters button
+          if (table.querySelectorAll('.filter-active').length) {
+            clearFiltersButton.classList.remove('hidden')
+          } else {
+            clearFiltersButton.classList.add('hidden')
           }
 
           // Invoke the table's pagination, returning it to page 1 and refreshing the results table.
@@ -56,11 +92,28 @@ function init() {
       }
     }
 
-    const filterButton = dateFilter.querySelector('.ems-date-filter__filter-button')
+    // Method for the clear filters button
+    const clearFilters = () => {
+      for (let dateField of startDateFields) {
+        dateField.value = ''
+      }
+      for (let dateField of endDateFields) {
+        dateField.value = ''
+      }
+    }
 
+    // Initialise the filter button
     filterButton.addEventListener('click', function (event) {
       event.preventDefault()
       applyFilter()
+    })
+
+    // Initialise the clear filters button
+    clearFiltersButton.addEventListener('click', function (event) {
+      event.preventDefault()
+      clearFilters()
+      applyFilter()
+      return false
     })
   }
 }
