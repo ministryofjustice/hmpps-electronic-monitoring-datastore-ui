4240152130f3e4ca814298d05310dc8ef94da92e
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-08-05 14:01:12 +0100

    Create contact-history, curfew-violations & suspensions routes that populate the timeline page with mock data via a mock API request.

---
+++ server/data/mockData/contactHistory.ts
@@ -0,0 +1,331 @@
+import { Events } from '../../interfaces/events'
+
+const contactHistory: Events = {
+  backUrl: '/order-summary',
+  events: [
+    {
+      name: 'Contact 1',
+      date: '01 January 2020',
+      records: [
+        {
+          key: 'Comments',
+          value: 'Outgoing Phone Call for (Left home during curfew Event) No Answer',
+        },
+        {
+          key: 'Comment Type',
+          value: 'Phone',
+        },
+        {
+          key: 'EventDesc',
+          value: 'Name',
+        },
+        {
+          key: 'Channel',
+          value: 'Mobile',
+        },
+        {
+          key: 'UserID',
+          value: 'nnnnnnn',
+        },
+        {
+          key: 'UserName',
+          value: 'Peter Parker',
+        },
+        {
+          key: 'HappenedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'HappenedTime',
+          value: '1110',
+        },
+        {
+          key: 'ModifiedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'ModifiedTime',
+          value: '1120',
+        },
+      ],
+    },
+    {
+      name: 'Contact 2',
+      date: '01 January 2021',
+      records: [
+        {
+          key: 'Comments',
+          value: 'Outgoing Phone Call for (Left home during curfew Event) No Answer',
+        },
+        {
+          key: 'Comment Type',
+          value: 'Phone',
+        },
+        {
+          key: 'EventDesc',
+          value: 'Name',
+        },
+        {
+          key: 'Channel',
+          value: 'Mobile',
+        },
+        {
+          key: 'UserID',
+          value: 'nnnnnnn',
+        },
+        {
+          key: 'UserName',
+          value: 'Peter Parker',
+        },
+        {
+          key: 'HappenedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'HappenedTime',
+          value: '1110',
+        },
+        {
+          key: 'ModifiedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'ModifiedTime',
+          value: '1120',
+        },
+      ],
+    },
+    {
+      name: 'Contact 3',
+      date: '01 January 2022',
+      records: [
+        {
+          key: 'Comments',
+          value: 'Outgoing Phone Call for (Left home during curfew Event) No Answer',
+        },
+        {
+          key: 'Comment Type',
+          value: 'Phone',
+        },
+        {
+          key: 'EventDesc',
+          value: 'Name',
+        },
+        {
+          key: 'Channel',
+          value: 'Mobile',
+        },
+        {
+          key: 'UserID',
+          value: 'nnnnnnn',
+        },
+        {
+          key: 'UserName',
+          value: 'Peter Parker',
+        },
+        {
+          key: 'HappenedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'HappenedTime',
+          value: '1110',
+        },
+        {
+          key: 'ModifiedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'ModifiedTime',
+          value: '1120',
+        },
+      ],
+    },
+    {
+      name: 'Contact 4',
+      date: '01 January 2023',
+      records: [
+        {
+          key: 'Comments',
+          value: 'Outgoing Phone Call for (Left home during curfew Event) No Answer',
+        },
+        {
+          key: 'Comment Type',
+          value: 'Phone',
+        },
+        {
+          key: 'EventDesc',
+          value: 'Name',
+        },
+        {
+          key: 'Channel',
+          value: 'Mobile',
+        },
+        {
+          key: 'UserID',
+          value: 'nnnnnnn',
+        },
+        {
+          key: 'UserName',
+          value: 'Peter Parker',
+        },
+        {
+          key: 'HappenedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'HappenedTime',
+          value: '1110',
+        },
+        {
+          key: 'ModifiedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'ModifiedTime',
+          value: '1120',
+        },
+      ],
+    },
+    {
+      name: 'Contact 5',
+      date: '01 January 2010',
+      records: [
+        {
+          key: 'Comments',
+          value: 'Outgoing Phone Call for (Left home during curfew Event) No Answer',
+        },
+        {
+          key: 'Comment Type',
+          value: 'Phone',
+        },
+        {
+          key: 'EventDesc',
+          value: 'Name',
+        },
+        {
+          key: 'Channel',
+          value: 'Mobile',
+        },
+        {
+          key: 'UserID',
+          value: 'nnnnnnn',
+        },
+        {
+          key: 'UserName',
+          value: 'Peter Parker',
+        },
+        {
+          key: 'HappenedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'HappenedTime',
+          value: '1110',
+        },
+        {
+          key: 'ModifiedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'ModifiedTime',
+          value: '1120',
+        },
+      ],
+    },
+    {
+      name: 'Contact 6',
+      date: '01 January 2011',
+      records: [
+        {
+          key: 'Comments',
+          value: 'Outgoing Phone Call for (Left home during curfew Event) No Answer',
+        },
+        {
+          key: 'Comment Type',
+          value: 'Phone',
+        },
+        {
+          key: 'EventDesc',
+          value: 'Name',
+        },
+        {
+          key: 'Channel',
+          value: 'Mobile',
+        },
+        {
+          key: 'UserID',
+          value: 'nnnnnnn',
+        },
+        {
+          key: 'UserName',
+          value: 'Peter Parker',
+        },
+        {
+          key: 'HappenedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'HappenedTime',
+          value: '1110',
+        },
+        {
+          key: 'ModifiedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'ModifiedTime',
+          value: '1120',
+        },
+      ],
+    },
+    {
+      name: 'Contact 7',
+      date: '01 January 2012',
+      records: [
+        {
+          key: 'Comments',
+          value: 'Outgoing Phone Call for (Left home during curfew Event) No Answer',
+        },
+        {
+          key: 'Comment Type',
+          value: 'Phone',
+        },
+        {
+          key: 'EventDesc',
+          value: 'Name',
+        },
+        {
+          key: 'Channel',
+          value: 'Mobile',
+        },
+        {
+          key: 'UserID',
+          value: 'nnnnnnn',
+        },
+        {
+          key: 'UserName',
+          value: 'Peter Parker',
+        },
+        {
+          key: 'HappenedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'HappenedTime',
+          value: '1110',
+        },
+        {
+          key: 'ModifiedDate',
+          value: '01.01.2020',
+        },
+        {
+          key: 'ModifiedTime',
+          value: '1120',
+        },
+      ],
+    },
+  ],
+}
+
+export default contactHistory
---
+++ server/data/mockData/suspensions.ts
@@ -0,0 +1,219 @@
+import { Events } from '../../interfaces/events'
+
+const suspensions: Events = {
+  backUrl: '/order-summary',
+  events: [
+    {
+      name: 'Event 1',
+      date: '01 January 2020',
+      records: [
+        {
+          key: 'StartDate',
+          value: '01.02.2020',
+        },
+        {
+          key: 'StartTime',
+          value: '1100',
+        },
+        {
+          key: 'EndDate',
+          value: '01.02.2020',
+        },
+        {
+          key: 'EndTime',
+          value: '1100',
+        },
+        {
+          key: 'StartEventDesc',
+          value: 'Out past curfew',
+        },
+        {
+          key: 'EndEventDesc',
+          value: 'Out at end of curfew',
+        },
+        {
+          key: 'AbsenceInSeconds',
+          value: '240',
+        },
+        {
+          key: 'AbsenceInHHMMSS',
+          value: '11.02.23',
+        },
+        {
+          key: 'ExcludeFromATV',
+          value: 'Yes',
+        },
+      ],
+    },
+    {
+      name: 'Event 2',
+      date: '01 January 2019',
+      records: [
+        {
+          key: 'StartDate',
+          value: '01.02.2020',
+        },
+        {
+          key: 'StartTime',
+          value: '1100',
+        },
+        {
+          key: 'EndDate',
+          value: '01.02.2020',
+        },
+        {
+          key: 'EndTime',
+          value: '1100',
+        },
+        {
+          key: 'StartEventDesc',
+          value: 'Out past curfew',
+        },
+        {
+          key: 'EndEventDesc',
+          value: 'Out at end of curfew',
+        },
+        {
+          key: 'AbsenceInSeconds',
+          value: '240',
+        },
+        {
+          key: 'AbsenceInHHMMSS',
+          value: '11.02.23',
+        },
+        {
+          key: 'ExcludeFromATV',
+          value: 'Yes',
+        },
+      ],
+    },
+    {
+      name: 'Event 3',
+      date: '01 January 2018',
+      records: [
+        {
+          key: 'StartDate',
+          value: '01.02.2020',
+        },
+        {
+          key: 'StartTime',
+          value: '1100',
+        },
+        {
+          key: 'EndDate',
+          value: '01.02.2020',
+        },
+        {
+          key: 'EndTime',
+          value: '1100',
+        },
+        {
+          key: 'StartEventDesc',
+          value: 'Out past curfew',
+        },
+        {
+          key: 'EndEventDesc',
+          value: 'Out at end of curfew',
+        },
+        {
+          key: 'AbsenceInSeconds',
+          value: '240',
+        },
+        {
+          key: 'AbsenceInHHMMSS',
+          value: '11.02.23',
+        },
+        {
+          key: 'ExcludeFromATV',
+          value: 'Yes',
+        },
+      ],
+    },
+    {
+      name: 'Event 4',
+      date: '01 January 2017',
+      records: [
+        {
+          key: 'StartDate',
+          value: '01.02.2020',
+        },
+        {
+          key: 'StartTime',
+          value: '1100',
+        },
+        {
+          key: 'EndDate',
+          value: '01.02.2020',
+        },
+        {
+          key: 'EndTime',
+          value: '1100',
+        },
+        {
+          key: 'StartEventDesc',
+          value: 'Out past curfew',
+        },
+        {
+          key: 'EndEventDesc',
+          value: 'Out at end of curfew',
+        },
+        {
+          key: 'AbsenceInSeconds',
+          value: '240',
+        },
+        {
+          key: 'AbsenceInHHMMSS',
+          value: '11.02.23',
+        },
+        {
+          key: 'ExcludeFromATV',
+          value: 'Yes',
+        },
+      ],
+    },
+    {
+      name: 'Event 5',
+      date: '01 January 2016',
+      records: [
+        {
+          key: 'StartDate',
+          value: '01.02.2020',
+        },
+        {
+          key: 'StartTime',
+          value: '1100',
+        },
+        {
+          key: 'EndDate',
+          value: '01.02.2020',
+        },
+        {
+          key: 'EndTime',
+          value: '1100',
+        },
+        {
+          key: 'StartEventDesc',
+          value: 'Out past curfew',
+        },
+        {
+          key: 'EndEventDesc',
+          value: 'Out at end of curfew',
+        },
+        {
+          key: 'AbsenceInSeconds',
+          value: '240',
+        },
+        {
+          key: 'AbsenceInHHMMSS',
+          value: '11.02.23',
+        },
+        {
+          key: 'ExcludeFromATV',
+          value: 'Yes',
+        },
+      ],
+    },
+  ],
+}
+
+export default suspensions
