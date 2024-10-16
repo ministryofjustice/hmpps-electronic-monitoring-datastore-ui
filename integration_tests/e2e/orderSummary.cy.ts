c12a403f7e9ffe364abe21e662d4a8697f7d0c16
Lee <lee_b250@hotmail.co.uk>
2024-10-16 16:38:53 +0100

    Add page models and tests for order summary

--- integration_tests/e2e/orderSummary.cy.ts
+++ integration_tests/e2e/orderSummary.cy.ts
@@ -6,7 +6,7 @@ context('Order Summary', () => {
     cy.task('reset')
     cy.task('stubSignIn')
     cy.signIn()
-    cy.visit('/orders/fake-order-id/summary')
+    cy.visit('/orders/summary')
   })
 
   it('is reachable', () => {
@@ -17,6 +17,99 @@ context('Order Summary', () => {
     it('contains row Legacy subject ID', () => {
       const summaryPage = Page.verifyOnPage(OrderSummaryPage)
       summaryPage.legacySubjectId().should('be.visible')
+    })
+
+    it('contains row name', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.name().should('be.visible')
+    })
+
+    it('contains row alias', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.alias().should('be.visible')
+    })
+
+    it('contains row date of birth', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.dateOfBirth().should('be.visible')
+    })
+
+    it('contains row postcode', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.postcode().should('be.visible')
+    })
+
+    it('contains row address', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.address().should('be.visible')
+    })
+
+    it('contains row tag type', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.tagType().should('be.visible')
+    })
+
+    it('contains row end date', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.endDate().should('be.visible')
+    })
+  })
+
+  describe('Grid buttons', () => {
+    it('contains button order details button an navigates to expected page', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.orderDetailsButton().should('be.visible')
+      summaryPage.orderDetailsButton().click()
+      cy.url().should('include', '/orders/details')
+    })
+
+    it('contains button visits and tasks', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.visitsAndTasksButton().should('be.visible')
+      summaryPage.visitsAndTasksButton().click()
+      cy.url().should('include', '/orders/visits-and-tasks')
+    })
+
+    it('contains button event history', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.eventHistoryButton().should('be.visible')
+      summaryPage.eventHistoryButton().click()
+      cy.url().should('include', '/orders/event-history')
+    })
+
+    it('contains button equipment details', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.equipmentDetailsButton().should('be.visible')
+      summaryPage.equipmentDetailsButton().click()
+      cy.url().should('include', '/orders/equipment-details')
+    })
+
+    it('contains button curfew hours', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.curfewHoursButton().should('be.visible')
+      summaryPage.curfewHoursButton().click()
+      cy.url().should('include', '/orders/curfew-hours')
+    })
+
+    it('contains button curfew violations', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.curfewViolationsButton().should('be.visible')
+      summaryPage.curfewViolationsButton().click()
+      cy.url().should('include', '/orders/curfew-violations')
+    })
+
+    it('contains button contact history', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.contactHistoryButton().should('be.visible')
+      summaryPage.contactHistoryButton().click()
+      cy.url().should('include', '/orders/contact-history')
+    })
+
+    it('contains button suspensions', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.suspensionsButton().should('be.visible')
+      summaryPage.suspensionsButton().click()
+      cy.url().should('include', '/orders/suspensions')
     })
   })
 })
--- integration_tests/pages/orderSummary.ts
+++ integration_tests/pages/orderSummary.ts
@@ -10,4 +10,34 @@ export default class OrderSummaryPage extends Page {
   }
 
   legacySubjectId = (): PageElement => cy.get('.govuk-table__row').children('th').contains('Legacy subject ID')
+
+  name = (): PageElement => cy.get('.govuk-table__row').children('th').contains('Name')
+
+  alias = (): PageElement => cy.get('.govuk-table__row').children('th').contains('Alias')
+
+  dateOfBirth = (): PageElement => cy.get('.govuk-table__row').children('th').contains('Date of birth')
+
+  postcode = (): PageElement => cy.get('.govuk-table__row').children('th').contains('Postcode')
+
+  address = (): PageElement => cy.get('.govuk-table__row').children('th').contains('Address')
+
+  tagType = (): PageElement => cy.get('.govuk-table__row').children('th').contains('Tag type')
+
+  endDate = (): PageElement => cy.get('.govuk-table__row').children('th').contains('End date')
+
+  orderDetailsButton = (): PageElement => cy.get('.ems-button-grid__button').contains('Order details')
+
+  visitsAndTasksButton = (): PageElement => cy.get('.ems-button-grid__button').contains('Visits and tasks')
+
+  eventHistoryButton = (): PageElement => cy.get('.ems-button-grid__button').contains('Event history')
+
+  equipmentDetailsButton = (): PageElement => cy.get('.ems-button-grid__button').contains('Equipment details')
+
+  curfewHoursButton = (): PageElement => cy.get('.ems-button-grid__button').contains('Curfew hours')
+
+  curfewViolationsButton = (): PageElement => cy.get('.ems-button-grid__button').contains('Curfew violations')
+
+  contactHistoryButton = (): PageElement => cy.get('.ems-button-grid__button').contains('Contact history')
+
+  suspensionsButton = (): PageElement => cy.get('.ems-button-grid__button').contains('Suspensions')
 }
