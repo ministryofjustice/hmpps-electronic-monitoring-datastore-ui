89ecebeeb6c9d29be9b4f6fe84af872b01797bcc
Lee-250 <71326033+Lee-250@users.noreply.github.com>
2024-10-16 17:47:31 +0100

    Adding UI tests (#14)
    * adding beginning of cypress tests for orderSummary
    
    * Add page models and tests for order summary
    
    * fixed the dummy orderId value - needs to be 1234567 as that's the value used in creation of the subsidary page urls when this page loads
    
    * somehow missed actually using the orderId in the back links - must have not saved the file before!
    
    ---------
    
    Co-authored-by: Jasper <54336703+JazJax@users.noreply.github.com>

---
+++ integration_tests/e2e/orderSummary.cy.ts
@@ -0,0 +1,117 @@
+import OrderSummaryPage from '../pages/orderSummary'
+import Page from '../pages/page'
+
+context('Order Summary', () => {
+  const orderId = '1234567'
+
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.signIn()
+    cy.visit(`/orders/${orderId}/summary`)
+  })
+
+  it('is reachable', () => {
+    Page.verifyOnPage(OrderSummaryPage)
+  })
+
+  describe('Order summary table', () => {
+    it('contains row Legacy subject ID', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.legacySubjectId().should('be.visible')
+    })
+
+    it('contains row name', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.name().should('be.visible')
+    })
+
+    it('contains row alias', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.alias().should('be.visible')
+    })
+
+    it('contains row date of birth', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.dateOfBirth().should('be.visible')
+    })
+
+    it('contains row postcode', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.postcode().should('be.visible')
+    })
+
+    it('contains row address', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.address().should('be.visible')
+    })
+
+    it('contains row tag type', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.tagType().should('be.visible')
+    })
+
+    it('contains row end date', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.endDate().should('be.visible')
+    })
+  })
+
+  describe('Grid buttons', () => {
+    it('contains button order details button an navigates to expected page', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.orderDetailsButton().should('be.visible')
+      summaryPage.orderDetailsButton().click()
+      cy.url().should('include', `/orders/${orderId}/details`)
+    })
+
+    it('contains button visits and tasks', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.visitsAndTasksButton().should('be.visible')
+      summaryPage.visitsAndTasksButton().click()
+      cy.url().should('include', `/orders/${orderId}/visits-and-tasks`)
+    })
+
+    it('contains button event history', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.eventHistoryButton().should('be.visible')
+      summaryPage.eventHistoryButton().click()
+      cy.url().should('include', `/orders/${orderId}/event-history`)
+    })
+
+    it('contains button equipment details', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.equipmentDetailsButton().should('be.visible')
+      summaryPage.equipmentDetailsButton().click()
+      cy.url().should('include', `/orders/${orderId}/equipment-details`)
+    })
+
+    it('contains button curfew hours', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.curfewHoursButton().should('be.visible')
+      summaryPage.curfewHoursButton().click()
+      cy.url().should('include', `/orders/${orderId}/curfew-hours`)
+    })
+
+    it('contains button curfew violations', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.curfewViolationsButton().should('be.visible')
+      summaryPage.curfewViolationsButton().click()
+      cy.url().should('include', `/orders/${orderId}/curfew-violations`)
+    })
+
+    it('contains button contact history', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.contactHistoryButton().should('be.visible')
+      summaryPage.contactHistoryButton().click()
+      cy.url().should('include', `/orders/${orderId}/contact-history`)
+    })
+
+    it('contains button suspensions', () => {
+      const summaryPage = Page.verifyOnPage(OrderSummaryPage)
+      summaryPage.suspensionsButton().should('be.visible')
+      summaryPage.suspensionsButton().click()
+      cy.url().should('include', `/orders/${orderId}/suspensions`)
+    })
+  })
+})
