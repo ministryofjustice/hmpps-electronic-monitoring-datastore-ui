befd9529caac2376588399d3856ce63196eb2558
Lee <lee_b250@hotmail.co.uk>
2024-10-24 17:07:57 +0100

    Add page components and tests for remaining pages

---
+++ integration_tests/e2e/contactHistory.cy.ts
@@ -0,0 +1,49 @@
+import ContactHistoryPage from '../pages/contactHistory'
+import Page, { PageElement } from '../pages/page'
+
+context('Contact history', () => {
+  const orderId = '1234567'
+
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.signIn()
+    cy.visit(`/orders/${orderId}/contact-history`)
+  })
+
+  it('is reachable', () => {
+    Page.verifyOnPage(ContactHistoryPage)
+  })
+
+  describe('timeline component', () => {
+    it('Renders an event timeline', () => {
+      const contactHistory = Page.verifyOnPage(ContactHistoryPage)
+      contactHistory.timeline().should('be.visible')
+    })
+  })
+
+  describe('Violations timeline item', () => {
+    it('Includes expected column headers', () => {
+      const contactHistory = Page.verifyOnPage(ContactHistoryPage)
+
+      contactHistory.timelineItems().each($item => {
+        cy.wrap($item).within(() => {
+          contactHistory.contactHistoryTableColumnHeaders('Data title').should('be.visible')
+          contactHistory.contactHistoryTableColumnHeaders('Data').should('be.visible')
+        })
+      })
+    })
+
+    it('Includes expected row headers', () => {
+      const contactHistory = Page.verifyOnPage(ContactHistoryPage)
+
+      contactHistory.timelineItems().each($item => {
+        cy.wrap($item).within(() => {
+          contactHistory.contactHistoryTableRowHeaders('Comments').should('be.visible')
+          contactHistory.contactHistoryTableRowHeaders('Comment Type').should('be.visible')
+          // Add row headers here when finalised.
+        })
+      })
+    })
+  })
+})
---
+++ integration_tests/e2e/orderDetails.cy.ts
@@ -0,0 +1,39 @@
+import OrderDetailsPage from '../pages/orderDetails'
+import Page, { PageElement } from '../pages/page'
+
+context('Order Details', () => {
+  const orderId = '1234567'
+
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.signIn()
+    cy.visit(`/orders/${orderId}/details`)
+  })
+
+  it('is reachable', () => {
+    Page.verifyOnPage(OrderDetailsPage)
+  })
+
+  describe('Order details table', () => {
+    it('Includes expected column headers', () => {
+      const orderDetailsPage = Page.verifyOnPage(OrderDetailsPage)
+
+      orderDetailsPage.orderDetailsTable().each(() => {
+        orderDetailsPage.orderDetailsColumnHeaders('Data title').should('be.visible')
+        orderDetailsPage.orderDetailsColumnHeaders('Data').should('be.visible')
+      })
+    })
+
+    it('Includes expected row headers', () => {
+      const orderDetailsPage = Page.verifyOnPage(OrderDetailsPage)
+
+      orderDetailsPage.orderDetailsTable().each(() => {
+        orderDetailsPage.orderDetailsRowHeaders('MAPPA').should('be.visible')
+        orderDetailsPage.orderDetailsRowHeaders('PPO').should('be.visible')
+        orderDetailsPage.orderDetailsRowHeaders('AgeCatNow').should('be.visible')
+        orderDetailsPage.orderDetailsRowHeaders('AliasName').should('be.visible')
+      })
+    })
+  })
+})
