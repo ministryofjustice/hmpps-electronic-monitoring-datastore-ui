d7ca89caafbe726d5755388cbdf6642ddacc4133
Lee <lee_b250@hotmail.co.uk>
2024-11-08 10:31:28 +0000

    Add device wearer table mock data and get function

---
+++ server/data/mockData/deviceWearer.ts
@@ -0,0 +1,101 @@
+import { Records } from '../../interfaces/records'
+
+const deviceWearer: Records = {
+  backUrl: '/order-summary',
+  records: [
+    {
+      key: 'Specials',
+      value: 'No',
+    },
+    {
+      key: 'Legacy Subject ID',
+      value: '2445156',
+    },
+    {
+      key: 'Legacy Order ID',
+      value: '2212126',
+    },
+    {
+      key: 'First name',
+      value: 'Peter',
+    },
+    {
+      key: 'Last name',
+      value: 'Parker',
+    },
+    {
+      key: 'Integrity name',
+      value: 'Peter(!)MEDIUM',
+    },
+    {
+      key: 'Alias',
+      value: 'Spiderman',
+    },
+    {
+      key: 'Date of birth',
+      value: '01.01.1960',
+    },
+    {
+      key: 'Adult/child',
+      value: 'Adult',
+    },
+    {
+      key: 'Legacy Sex',
+      value: 'Male',
+    },
+    {
+      key: 'Contact',
+      value: 'Norman Osborn',
+    },
+    {
+      key: 'Primary Address',
+      value: 'Address line 1',
+    },
+    {
+      key: 'Phone/mobile number',
+      value: '01234567890',
+    },
+    {
+      key: 'PPO',
+      value: 'Yes',
+    },
+    {
+      key: 'MAPPA',
+      value: 'MAPPA 1',
+    },
+    {
+      key: 'Technical bail',
+      value: 'Yes',
+    },
+    {
+      key: 'Manual Risk',
+      value: 'CONCURRENT 27/05/21 MEDIUM ASSAULT ON PC',
+    },
+    {
+      key: 'Offence Risk',
+      value: 'True',
+    },
+    {
+      key: 'Postcode Risk',
+      value: 'Yes',
+    },
+    {
+      key: 'False Limb Risk',
+      value: 'FMO VISIT 02/06/20 LOW NO RISK CONCERNS',
+    },
+    {
+      key: 'Migrated Risk',
+      value: 'DOUBLE CREW REQUIRED SUBJECTS FATHER CAN BE ABUSIVE AGGRESSIVE',
+    },
+    {
+      key: 'Range Risk',
+      value: 'DOUBLE CREW REQUIRED - early visits requested if possible',
+    },
+    {
+      key: 'Report Risk',
+      value: 'Risk',
+    },
+  ],
+}
+
+export default deviceWearer
