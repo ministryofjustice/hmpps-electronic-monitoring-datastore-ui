ea383b8fe58aba6d45b9f78b7cb4b6fb028652cc
mattonfoot <matt.smith@justice.gov.uk>
2025-01-23 18:45:18 +0000

    Add event controller and views

--- server/routes/orderRouter.ts
+++ server/routes/orderRouter.ts
@@ -7,23 +7,29 @@ import getCurfewHours from '../data/getCurfewHours'
 import getCurfewViolations from '../data/getCurfewViolations'
 import getHmuEquipmentDetails from '../data/getHmuEquipmentDetails'
 import getDeviceEquipmentDetails from '../data/getDeviceEquipmentDetails'
-import getEventHistory from '../data/getEventHistory'
 import getOrderDetails from '../data/getOrderDetails'
 import getDeviceWearerDetails from '../data/getDeviceWearer'
 import getSuspensionOfVisits from '../data/getSuspensionOfVisits'
-import tabluateRecords from '../utils/tabulateRecords'
+import tabulateRecords from '../utils/tabulateRecords'
 import type { Services } from '../services'
 import { Page } from '../services/auditService'
 import OldOrderController from './orderController'
 import OrderController from '../controllers/orderController'
+import EventsController from '../controllers/eventsController'
 import getVisitDetails from '../data/getVisitDetails'
 
-export default function orderRouter({ auditService, orderService, datastoreOrderService }: Services): Router {
+export default function orderRouter({
+  auditService,
+  orderService,
+  datastoreOrderService,
+  eventsService,
+}: Services): Router {
   const router = Router({ mergeParams: true })
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   const oldOrderController = new OldOrderController(auditService, orderService)
   const orderController = new OrderController(auditService, datastoreOrderService)
+  const eventsController = new EventsController(auditService, eventsService)
 
   // TODO: Deprecate in favour of /summary
   get('/information', async (req, res, next) => oldOrderController.getSummary(req, res))
@@ -55,8 +61,8 @@ export default function orderRouter({ auditService, orderService, datastoreOrder
       const orderDetails = await getOrderDetails()
       const deviceWearerDetails = await getDeviceWearerDetails()
 
-      const tabulatedOrderDetails = tabluateRecords(orderDetails, 'Order Data')
-      const tabulatedDeviceWearerDetails = tabluateRecords(deviceWearerDetails, 'Device Wearer Data')
+      const tabulatedOrderDetails = tabulateRecords(orderDetails, 'Order Data')
+      const tabulatedDeviceWearerDetails = tabulateRecords(deviceWearerDetails, 'Device Wearer Data')
 
       res.render('pages/orderDetails', {
         deviceWearer: tabulatedDeviceWearerDetails,
@@ -97,8 +103,8 @@ export default function orderRouter({ auditService, orderService, datastoreOrder
       const hmuEquipmentDetails = await getHmuEquipmentDetails()
       const deviceEquipmentDetails = await getDeviceEquipmentDetails()
 
-      const tabulatedHmuEquipmentDetails = tabluateRecords(hmuEquipmentDetails, 'Equipment details')
-      const tabulatedDeviceEquipmentDetails = tabluateRecords(deviceEquipmentDetails, 'Equipment details')
+      const tabulatedHmuEquipmentDetails = tabulateRecords(hmuEquipmentDetails, 'Equipment details')
+      const tabulatedDeviceEquipmentDetails = tabulateRecords(deviceEquipmentDetails, 'Equipment details')
 
       res.render('pages/equipmentDetails', {
         hmuEquipmentDetails: tabulatedHmuEquipmentDetails,
@@ -116,25 +122,14 @@ export default function orderRouter({ auditService, orderService, datastoreOrder
     try {
       const { orderId } = req.params
       const curfewHours = await getCurfewHours()
-      const tabulatedCurfewHours = tabluateRecords(curfewHours, 'Curfew hours')
+      const tabulatedCurfewHours = tabulateRecords(curfewHours, 'Curfew hours')
       res.render('pages/twoColumnTable', { data: tabulatedCurfewHours, backUrl: `/orders/${orderId}/information` })
     } catch {
       res.status(500).send('Error fetching data')
     }
   })
 
-  get('/event-history', async (req, res, next) => {
-    await auditService.logPageView(Page.EVENT_HISTORY_PAGE, { who: res.locals.user.username, correlationId: req.id })
-
-    try {
-      const { orderId } = req.params
-      const eventHistory = await getEventHistory()
-      const timeline = createTimeline(eventHistory, 'Event history')
-      res.render('pages/timeline', { data: timeline, backUrl: `/orders/${orderId}/information` })
-    } catch {
-      res.status(500).send('Error fetching data')
-    }
-  })
+  get('/event-history', eventsController.showHistory)
 
   get('/suspension-of-visits', async (req, res, next) => {
     await auditService.logPageView(Page.SUSPENSION_OF_VISITS_PAGE, {
