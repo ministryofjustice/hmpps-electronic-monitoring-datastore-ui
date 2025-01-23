ff22609516b6d1dfcb0ae08f51547746660c659e
mattonfoot <matt.smith@justice.gov.uk>
2025-01-23 18:41:14 +0000

    Add event service and initialise

---
+++ server/services/eventsService.ts
@@ -0,0 +1,39 @@
+import logger from '../../logger'
+import getSanitisedError from '../sanitisedError'
+
+import DatastoreClient from '../data/datastoreClient'
+import { HmppsAuthClient, RestClientBuilder } from '../data'
+
+import { OrderRequest } from '../types/OrderRequest'
+import { ContactEvent } from '../models/contactEvents'
+import { IncidentEvent } from '../models/incidentEvents'
+import { MonitoringEvent } from '../models/monitoringEvents'
+
+export default class EventsService {
+  private readonly datastoreClient: DatastoreClient
+
+  constructor(
+    private readonly datastoreClientFactory: RestClientBuilder<DatastoreClient>,
+    private readonly hmppsAuthClient: HmppsAuthClient,
+  ) {
+    this.datastoreClient = this.datastoreClientFactory('uninitialized')
+  }
+
+  async getEvents(input: OrderRequest): Promise<(ContactEvent | IncidentEvent | MonitoringEvent)[]> {
+    this.datastoreClient.updateToken(input.accessToken)
+
+    let events = []
+    try {
+      events = await Promise.all([
+        this.datastoreClient.getMonitoringEvents(input),
+        this.datastoreClient.getIncidentEvents(input),
+        this.datastoreClient.getContactHistory(input),
+      ])
+    } catch (error) {
+      logger.error(getSanitisedError(error), 'Error retrieving list of events')
+      return error
+    }
+
+    return events.flat()
+  }
+}
