dce820656a27f0d2400a2ea010db0b8af0d8dd86
mattonfoot <matt.smith@justice.gov.uk>
2025-01-23 18:41:14 +0000

    Add event service and initialise

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,6 +1,7 @@
 import { dataAccess } from '../data'
 import AuditService from './auditService'
 import OrderService from './orderService'
+import EventsService from './eventsService'
 import DatastoreOrderService from './datastoreOrderService'
 import DatastoreSearchService from './datastoreSearchService'
 
@@ -10,9 +11,10 @@ export const services = () => {
   const auditService = new AuditService(hmppsAuditClient)
   const datastoreSearchService = new DatastoreSearchService(datastoreClientFactory, hmppsAuthClient)
 
-  // TODO: Deprecate order servive in favour of DataStoreOrderService
+  // TODO: Deprecate order service in favour of DataStoreOrderService
   const orderService = new OrderService()
   const datastoreOrderService = new DatastoreOrderService(datastoreClientFactory, hmppsAuthClient)
+  const eventsService = new EventsService(datastoreClientFactory, hmppsAuthClient)
 
   return {
     applicationInfo,
@@ -20,8 +22,9 @@ export const services = () => {
     orderService,
     datastoreOrderService,
     datastoreSearchService,
+    eventsService,
   }
 }
 
 export type Services = ReturnType<typeof services>
-export { AuditService, DatastoreOrderService, DatastoreSearchService }
+export { AuditService, DatastoreOrderService, DatastoreSearchService, EventsService }
