159f45e5f9035829e2bfd89cf46e644279c4b6ce
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2025-01-29 13:57:44 +0000

    Correct date of birth field name in orderDetails model.

--- server/models/orderDetails.ts
+++ server/models/orderDetails.ts
@@ -35,151 +35,151 @@ const orderDetails = z.object({
   wearingWristPid: z.string().nullable(),
   notifyingOrganisationDetailsName: z.string().nullable(),
   responsibleOrganisation: z.string().nullable(),
-  responsibleOrganisationDetailsRegion: z.string().nullable()
+  responsibleOrganisationDetailsRegion: z.string().nullable(),
 })
 
-const formatOrderDetails = orderDetails.transform((data) => {
+const formatOrderDetails = orderDetails.transform(data => {
   return {
     deviceWearerData: [
       {
         key: 'Specials',
-        value: data.specials
+        value: data.specials,
       },
       {
         key: 'Legacy subject ID',
-        value: data.legacySubjectId
+        value: data.legacySubjectId,
       },
       {
         key: 'Legacy order ID',
-        value: data.legacyOrderId
+        value: data.legacyOrderId,
       },
       {
         key: 'First name',
-        value: data.firstName
+        value: data.firstName,
       },
       {
         key: 'Last name',
-        value: data.lastName
+        value: data.lastName,
       },
       {
         key: 'Alias',
-        value: data.alias
+        value: data.alias,
+      },
+      {
+        key: 'Date of birth',
+        value: data.dateOfBirth,
       },
       {
         key: 'Adult/child',
-        value: data.dateOfBirth
-      },
-      {
-        key: 'Adult/child',
-        value: data.adultOrChild
+        value: data.adultOrChild,
       },
       {
         key: 'Legacy sex',
-        value: data.sex
+        value: data.sex,
       },
       {
         key: 'Contact',
-        value: data.contact
+        value: data.contact,
       },
       {
         key: 'Primary address line 1',
-        value: data.primaryAddressLine1
+        value: data.primaryAddressLine1,
       },
       {
         key: 'Primary address line 2',
-        value: data.primaryAddressLine2
+        value: data.primaryAddressLine2,
       },
       {
         key: 'Primary address line 3',
-        value: data.primaryAddressLine3
+        value: data.primaryAddressLine3,
       },
       {
         key: 'Primary address line 4',
-        value: data.primaryAddressPostCode
+        value: data.primaryAddressPostCode,
       },
       {
         key: 'Phone/mobile number',
-        value: data.phoneOrMobileNumber
+        value: data.phoneOrMobileNumber,
       },
       {
         key: 'PPO',
-        value: data.ppo
+        value: data.ppo,
       },
       {
         key: 'MAPPA',
-        value: data.mappa
+        value: data.mappa,
       },
       {
         key: 'Technical bail',
-        value: data.technicalBail
+        value: data.technicalBail,
       },
       {
         key: 'Manual risk',
-        value: data.manualRisk
+        value: data.manualRisk,
       },
       {
         key: 'Offence risk',
-        value: data.offenceRisk ? 'Yes' : 'No'
+        value: data.offenceRisk ? 'Yes' : 'No',
       },
       {
         key: 'Postcode risk',
-        value: data.postCodeRisk
+        value: data.postCodeRisk,
       },
       {
         key: 'False limb risk',
-        value: data.falseLimbRisk
+        value: data.falseLimbRisk,
       },
       {
         key: 'Migrated risk',
-        value: data.migratedRisk
+        value: data.migratedRisk,
       },
       {
         key: 'Range risk',
-        value: data.rangeRisk
+        value: data.rangeRisk,
       },
       {
         key: 'Report risk',
-        value: data.reportRisk
-      }
+        value: data.reportRisk,
+      },
     ],
     orderData: [
       {
         key: 'Order start date',
-        value: data.orderStartDate
+        value: data.orderStartDate,
       },
       {
         key: 'Order end date',
-        value: data.orderEndDate
+        value: data.orderEndDate,
       },
       {
         key: 'Order type',
-        value: data.orderType
+        value: data.orderType,
       },
       {
         key: 'Order type description',
-        value: data.orderTypeDescription
+        value: data.orderTypeDescription,
       },
       {
         key: 'Order type detail',
-        value: data.orderTypeDetail
+        value: data.orderTypeDetail,
       },
       {
         key: 'Wearing wrist PID',
-        value: data.wearingWristPid
+        value: data.wearingWristPid,
       },
       {
         key: 'Notifying organisation name',
-        value: data.notifyingOrganisationDetailsName
+        value: data.notifyingOrganisationDetailsName,
       },
       {
         key: 'Responsible organisation',
-        value: data.responsibleOrganisation
+        value: data.responsibleOrganisation,
       },
       {
         key: 'Responsible organsiation region',
-        value: data.responsibleOrganisationDetailsRegion
-      }
-    ]
+        value: data.responsibleOrganisationDetailsRegion,
+      },
+    ],
   }
 })
 
