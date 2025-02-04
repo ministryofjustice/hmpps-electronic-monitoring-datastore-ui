79ff9f4a136b00814f2d24ff175f54bb4eb96735
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2025-02-04 12:04:17 +0000

    Create suspension of visits service, with unit tests.

---
+++ server/services/suspensionOfVisitsService.ts
@@ -0,0 +1,32 @@
+import logger from '../../logger'
+import getSanitisedError from '../sanitisedError'
+
+import DatastoreClient from '../data/datastoreClient'
+import { HmppsAuthClient, RestClientBuilder } from '../data'
+
+import { OrderRequest } from '../types/OrderRequest'
+import { SuspensionOfVisitsEvent } from '../models/suspensionOfVisitsEvents'
+
+export default class SuspensionOfVisitsService {
+  private readonly datastoreClient: DatastoreClient
+
+  constructor(
+    private readonly datastoreClientFactory: RestClientBuilder<DatastoreClient>,
+    private readonly hmppsAuthClient: HmppsAuthClient,
+  ) {
+    this.datastoreClient = this.datastoreClientFactory('uninitialized')
+  }
+
+  async getSuspensionOfVisitsEvents(input: OrderRequest): Promise<SuspensionOfVisitsEvent[]> {
+    this.datastoreClient.updateToken(input.accessToken)
+
+    try {
+      this.datastoreClient.updateToken(input.accessToken)
+      const result = await this.datastoreClient.getSuspensionOfVisits(input)
+      return result
+    } catch (error) {
+      logger.error(getSanitisedError(error), 'Error retrieving suspension of visits data')
+      throw error
+    }
+  }
+}
