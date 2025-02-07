59cff2449891386918a615d5773100ba4173011a
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2025-02-07 15:53:30 +0000

    Update suspensionOfVisits view model to format data more appropriately for the view. Update associated unit tests.

--- server/models/view-models/suspensionOfVisits.ts
+++ server/models/view-models/suspensionOfVisits.ts
@@ -1,21 +1,43 @@
 import { SuspensionOfVisitsEvent } from '../suspensionOfVisits'
 
+export type SuspensionOfVisitsViewEvent = {
+  timestamp: string
+  suspensionOfVisits: string
+  requestedDate: string
+  startDate: string
+  endDate: string
+}
+
 export type SuspensionOfVisitsViewModel = {
   orderId: number
   backUrl: string
-  events: SuspensionOfVisitsEvent[]
+  events: SuspensionOfVisitsViewEvent[]
 }
 
-const createViewModelFromApiDto = (orderId: number, events: SuspensionOfVisitsEvent[]): SuspensionOfVisitsViewModel => {
-  const model = {
-    orderId,
-    backUrl: `/orders/${orderId}`,
-    events: events.sort(
+const extractDate = (dateTime: string | null) => (dateTime ? new Date(dateTime).toLocaleDateString('en-UK') : '')
+
+const processEvents = (events: SuspensionOfVisitsEvent[]): SuspensionOfVisitsViewEvent[] =>
+  events
+    .sort(
       (a, b) =>
         new Date(a.suspensionOfVisitsRequestedDate).getTime() - new Date(b.suspensionOfVisitsRequestedDate).getTime(),
-    ),
+    )
+    .map(event => {
+      return {
+        timestamp: event.suspensionOfVisitsRequestedDate,
+        suspensionOfVisits: event.suspensionOfVisits,
+        requestedDate: extractDate(event.suspensionOfVisitsRequestedDate),
+        startDate: extractDate(event.suspensionOfVisitsStartDate),
+        endDate: extractDate(event.suspensionOfVisitsEndDate),
+      }
+    })
+
+const createViewModelFromApiDto = (orderId: number, events: SuspensionOfVisitsEvent[]): SuspensionOfVisitsViewModel => {
+  return {
+    orderId,
+    backUrl: `/orders/${orderId}`,
+    events: processEvents(events),
   }
-  return model
 }
 
 const construct = (orderId: number, events: SuspensionOfVisitsEvent[] = []): SuspensionOfVisitsViewModel => {
