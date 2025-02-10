e8721482ab0dfa5f4ed574952d23467221b163b7
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2025-02-10 11:10:14 +0000

    Extend emsDateFilter component to work with the MoJ timeline component.

--- assets/js/components/emsDateFilter.js
+++ assets/js/components/emsDateFilter.js
@@ -6,12 +6,19 @@ function init() {
   for (let dateFilter of emsDateFilters) {
     const filterButton = dateFilter.querySelector('.ems-date-filter__filter-button')
     const clearFilterButton = dateFilter.querySelector('.ems-date-filter__clear-filter-button')
-
     const startDateFields = dateFilter.querySelector('#start-date').querySelectorAll('input')
     const endDateFields = dateFilter.querySelector('#end-date').querySelectorAll('input')
     const errorMessage = dateFilter.querySelector('.govuk-error-message')
-
     const filterableTables = document.querySelectorAll('.date-filterable')
+    const timelineItems = document.querySelectorAll('.moj-timeline__item')
+
+    const filterElement = (element, startDate, endDate, filterDate) => {
+      if ((startDate == null || filterDate > startDate) && (endDate == null || filterDate < endDate)) {
+        element.classList.remove('filter-active')
+      } else {
+        element.classList.add('filter-active')
+      }
+    }
 
     const applyFilter = () => {
       const startDateInputs = {
@@ -58,24 +65,22 @@ function init() {
 
         for (let table of filterableTables) {
           const rows = table.querySelectorAll('[data-filter-date]')
-
           for (let row of rows) {
             const filterDate = parseInt(row.dataset.filterDate)
-
-            if ((startDate == null || filterDate > startDate) && (endDate == null || filterDate < endDate)) {
-              row.classList.remove('filter-active')
-            } else {
-              row.classList.add('filter-active')
-            }
+            filterElement(row, startDate, endDate, filterDate)
           }
-
-          if (startDate || endDate) {
-            clearFilterButton.classList.remove('hidden')
-          } else {
-            clearFilterButton.classList.add('hidden')
-          }
-
           table.getElementsByClassName('moj-pagination__item--link')[0].click()
+        }
+
+        for (let item of timelineItems) {
+          const filterDate = item.querySelector('time').dateTime.split('T')[0].split('-').join('')
+          filterElement(item, startDate, endDate, filterDate)
+        }
+
+        if (startDate || endDate) {
+          clearFilterButton.classList.remove('hidden')
+        } else {
+          clearFilterButton.classList.add('hidden')
         }
       }
     }
