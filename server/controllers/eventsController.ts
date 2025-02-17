d01292c918cf7fc3247555a424a643d6ecd679a8
mattonfoot <matt.smith@justice.gov.uk>
2025-02-17 14:34:29 +0000

    Fix use of accessToken over userToken

--- server/controllers/eventsController.ts
+++ server/controllers/eventsController.ts
@@ -19,7 +19,7 @@ export default class EventsController {
     const { orderId } = req.params
 
     const events = await this.eventsService.getEvents({
-      userToken: res.locals.user.token,
+      accessToken: res.locals.user.token,
       orderId,
     })
 
