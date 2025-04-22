b63666a97b4d82cdf066b966e7471375145b67db
mattonfoot <matt.smith@justice.gov.uk>
2025-04-22 23:12:43 +0100

    integrity order details

---
+++ server/models/integrity/orderDetails.ts
@@ -0,0 +1,38 @@
+import z from 'zod'
+
+export const IntegrityOrderDetailsModel = z.object({
+  legacySubjectId: z.string(),
+  firstName: z.string().nullable(),
+  lastName: z.string().nullable(),
+  alias: z.string().nullable(),
+  dateOfBirth: z.string().nullable(),
+  adultOrChild: z.string().nullable(),
+  sex: z.string().nullable(),
+  contact: z.string().nullable(),
+  primaryAddressLine1: z.string().nullable(),
+  primaryAddressLine2: z.string().nullable(),
+  primaryAddressLine3: z.string().nullable(),
+  primaryAddressPostCode: z.string().nullable(),
+  phoneOrMobileNumber: z.string().nullable(),
+  ppo: z.string().nullable(),
+  mappa: z.string().nullable(),
+  technicalBail: z.string().nullable(),
+  manualRisk: z.string().nullable(),
+  offenceRisk: z.boolean().nullable(),
+  postCodeRisk: z.string().nullable(),
+  falseLimbRisk: z.string().nullable(),
+  migratedRisk: z.string().nullable(),
+  rangeRisk: z.string().nullable(),
+  reportRisk: z.string().nullable(),
+  orderStartDate: z.string().nullable(),
+  orderEndDate: z.string().nullable(),
+  orderType: z.string().nullable(),
+  orderTypeDescription: z.string().nullable(),
+  orderTypeDetail: z.string().nullable(),
+  wearingWristPid: z.string().nullable(),
+  notifyingOrganisationDetailsName: z.string().nullable(),
+  responsibleOrganisation: z.string().nullable(),
+  responsibleOrganisationDetailsRegion: z.string().nullable(),
+})
+
+export type IntegrityOrderDetails = z.infer<typeof IntegrityOrderDetailsModel>
