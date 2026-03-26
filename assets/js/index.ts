a13b2bbe1660714f2024096793d3c58d6c50919c
mattonfoot <matt.smith@justice.gov.uk>
2026-03-26 15:31:46 +0000

    Fix setup for esbuild

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -1,5 +1,7 @@
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
+import * as ems from './ems.js'
 
 govukFrontend.initAll()
 mojFrontend.initAll()
+ems.initAll()
