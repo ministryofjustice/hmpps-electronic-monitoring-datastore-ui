c7f0c90237cba6eb62201d3f5ae293017c9ad0d3
mattonfoot <matt.smith@justice.gov.uk>
2026-09-19 06:26:52 +0100

    Add rate limiting config to raise integration test thresholds

--- server/middleware/setUpRateLimiting.ts
+++ server/middleware/setUpRateLimiting.ts
@@ -1,14 +1,12 @@
 import express, { Router } from 'express'
-import RateLimit from 'express-rate-limit'
+import RateLimit, { type Options } from 'express-rate-limit'
+import config from '../config'
 
 export default function setUpWebSession(): Router {
   const router = express.Router()
 
   // set up rate limiter: maximum of 30 requests per minute
-  const limiter = RateLimit({
-    windowMs: 60 * 1000, // 1 minute
-    limit: 30, // max 30 requests per windowMs
-  })
+  const limiter = RateLimit(config.rateLimiting as Partial<Options>)
 
   // apply rate limiter to all requests
   router.use(limiter)
